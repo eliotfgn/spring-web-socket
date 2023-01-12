1a0ef915ae0bfb5cff831d4c733e6210f928cd73
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-12 23:38:55 +0100

    initial commit

---
+++ src/main/java/com/eliotfgn/springwebsocket/SpringWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.eliotfgn.springwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliotfgn/springwebsocket/SpringWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliotfgn.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
